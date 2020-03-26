9e1af8b5e21a3aa9c2ddcdba82eee3086708b9d4
farookjintha <farookjintha@gmail.com>
2020-03-26 23:53:54 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const keys = require('./keys');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log('Lost PG Connection'));
+
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
